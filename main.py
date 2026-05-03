ceb3e96c6ea3c473e320638dccad2afb7c082df2
jhonny5 <jhorguma@gmail.com>
2026-05-02 23:15:59 -0500

    crear cliente en lista y listar clientes guardados

--- main.py
+++ main.py
@@ -23,28 +23,35 @@ ciudades = {
 #reto, devolver la hora en formato de 24 horas
 #esta editado para regresra horas
 @app.get("/hora/{iso_code}")
-def hora(iso_code: str):
+async def hora(iso_code: str):
     iso = iso_code.upper()
     zona_lugar = ciudades.get(iso)
     tz = zoneinfo.ZoneInfo(zona_lugar)
     return{"Hora": datetime.now(tz)}
 
-#codigo para id
+#codigo para id, despues se eliminara esta variable por la lista de clientes
 cliente_id:int = 0
-
+#crear una lista para guardar datos
+lista_clientes:list[Cliente] = []
 
 #es importante post(sin id), y get(con id)
 @app.post("/clientes", response_model=Cliente)
-def crear_cliente(datos_cliente: ClienteCrear):
+async def crear_cliente(datos_cliente: ClienteCrear):
     #aqui incrementamos id, y validar datos ingresados (simulando BD)
     cliente_val = Cliente.model_validate(datos_cliente.model_dump())
-    cliente_val.id = cliente_id + 1
+    #cliente_val.id = cliente_id + 1
+    cliente_val.id = len(lista_clientes)+1
+    lista_clientes.append(cliente_val)
     return cliente_val#datos_cliente
 
+@app.get("/clientes")
+async def listar_clientes():
+    return lista_clientes
+
 @app.post("/transacciones")
-def crear_transaccion(datos_transaccion: Transaccion):
+async def crear_transaccion(datos_transaccion: Transaccion):
     return datos_transaccion
 
 @app.post("/facturas")
-def crear_factura(datos_factura: Factura):
+async def crear_factura(datos_factura: Factura):
     return datos_factura
